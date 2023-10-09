3b8716b6082fcdc0e98ef90838e68a9ae780f233
yassinex02 <tahayassinemoumni@gmail.com>
2023-10-09 17:10:53 +0200

    fixing xssex

--- xssex.py
+++ xssex.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template_string
+from flask import Flask, request, render_template_string, escape
 
 app = Flask(__name__)
 
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def index():
     name = request.args.get('name', 'World')
     # Vulnerable to XSS
-    return render_template_string(f'Hello, {name}!')
+    return render_template_string('Hello ', escape(name), '!')
 
 
 if __name__ == "__main__":
